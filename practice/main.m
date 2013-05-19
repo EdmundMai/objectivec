5116b0404156346e3afa28949ed0a534908500f4
Edmund Mai <edmundmai@gmail.com>
2013-05-19 17:13:38 -0400

    Initial Commit

---
+++ practice/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  practice
+//
+//  Created by Edmund Mai on 5/19/13.
+//  Copyright (c) 2013 Edmund Mai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
