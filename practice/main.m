96e8a57df6e251290851861505ff551fb6f3e6b9
Edmund Mai <edmundmai@gmail.com>
2013-05-19 18:36:35 -0400

    first commit

--- practice/main.m
+++ practice/main.m
@@ -8,13 +8,61 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Fraction : NSObject
+
+- (void) print;
+- (void) setNumerator: (int) n;
+- (void) setDenominator: (int) d;
+
+@end
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+}
+
+- (void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+- (void) setNumerator: (int) n
+{
+    numerator = n;
+}
+
+- (void) setDenominator: (int) n
+{
+    denominator = n;
+}
+
+- (int) numerator
+{
+    return numerator;
+}
+
+- (int) denominator
+{
+    return denominator;
+}
+
+
+@end
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        Fraction *fract;
+        
+        fract = [[Fraction alloc] init];
+        
+        [fract setNumerator: 5];
+        [fract setDenominator: 10];
+        
+        [fract print];
         
     }
     return 0;
